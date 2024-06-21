e2aeed406fda4701e2a7c7e2fbeb3c255eb61abb
Traman2 <tejassraman@gmail.com>
2024-06-20 20:00:43 -0500

    Added methods to stack data structure

--- Stack.cpp
+++ Stack.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Stack.h"
+
+
+Stack::Stack() {
+
+}
+
+Stack::Stack(int initCap) {
+
+}
+
+bool Stack::isEmpty() {
+    return false;
+}
+
+int Stack::peek() {
+    return 0;
+}
+
+int Stack::pop() {
+    return 0;
+}
+
+void Stack::push(int item) {
+
+}
+
+void Stack::doubleCapacity() {
+
+}
+
+ostream& operator<<(ostream& stream, const Stack& obj) {
+
+}
--- Stack.h
+++ Stack.h
@@ -5,10 +5,29 @@
 #ifndef TOWEROFHANOI_STACK_H
 #define TOWEROFHANOI_STACK_H
 
+#include <cstddef>
+#include <ostream>
+
+using namespace std;
 
 class Stack {
+public:
+    Stack();
+    Stack(int initCap);
+
+    bool isEmpty();
+    int peek();
+    int pop();
+    void push(int item);
+
+private:
+    size_t size;
+    int values[];
+
+    void doubleCapacity();
 
 };
 
+ostream& operator<<(ostream& stream, const Stack& obj);
 
 #endif //TOWEROFHANOI_STACK_H
