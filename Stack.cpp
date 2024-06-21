e51b510c0f84e80b36be73def6f05c6e0a5dc98f
Traman2 <tejassraman@gmail.com>
2024-06-20 19:43:34 -0500

    Creation of Stack Data Structure to implement the towers

---
+++ Stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tejas on 6/20/2024.
+//
+
+#include "Stack.h"
---
+++ Stack.h
@@ -0,0 +1,14 @@
+//
+// Created by tejas on 6/20/2024.
+//
+
+#ifndef TOWEROFHANOI_STACK_H
+#define TOWEROFHANOI_STACK_H
+
+
+class Stack {
+
+};
+
+
+#endif //TOWEROFHANOI_STACK_H
